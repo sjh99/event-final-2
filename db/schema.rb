25cd6dcc0d32929f29e3d0ec3a91b2de0c2ad66b
Stephanie Hass <stephie.hass@kellogg.northwestern.edu>
2017-06-10 16:17:56 -0700

    stephie 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610024336) do
+ActiveRecord::Schema.define(version: 20170610024353) do
+
+  create_table "cities", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "event_listings", force: :cascade do |t|
     t.string   "event_name"
